e16ce44e6bf010e01675c1311eb9844bc285977d
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2024-08-08 14:29:24 -0500

    Time: 28 ms (96.50%) | Memory: 16.7 MB (26.50%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         # iterate through all strings and each individual character 
-        # 
-        
-        longest_prefix = strs[0]
-        current_prefix = strs[0]
+        # have a current substring and a longest substring
+
+        longest_substring = strs[0]
+        current_substring = strs[0]
 
         for i in range(1, len(strs)):
-            current_prefix = ""
-            for j in range(min(len(strs[i]), len(longest_prefix))):
-                if strs[i][j] == longest_prefix[j]:
-                    current_prefix += strs[i][j]
+            current_substring = ""
+            for j in range(min(len(strs[i]), len(longest_substring))):
+                if longest_substring[j] == strs[i][j]:
+                    current_substring += strs[i][j]
                 else:
                     break
             
-            if len(longest_prefix) > len(current_prefix):
-                longest_prefix = current_prefix
+            if len(longest_substring) > len(current_substring):
+                longest_substring = current_substring
+            
+        return longest_substring
         
-        return longest_prefix
+        
