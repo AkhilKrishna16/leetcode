c5018abe2a10cdba14d7593a47e7eeb6b8ba4d83
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2024-08-09 21:13:33 -0500

    Time: 36 ms (50.50%) | Memory: 16.5 MB (75.96%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -4,48 +4,49 @@ class Solution:
         # switch directions based on indices and where overflow is
 
         UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
-        direction = RIGHT
         ans = []
         i = 0
         j = 0
-        RIGHT_WALL = len(matrix[0])
-        DOWN_WALL = len(matrix)
+        RIGHT_WALL = len(matrix[0]) - 1
         LEFT_WALL = 0
         UP_WALL = 1
+        DOWN_WALL = len(matrix) - 1
+        direction = RIGHT
 
         while len(ans) != len(matrix) * len(matrix[0]):
             if direction == RIGHT:
-                while j < RIGHT_WALL:
+                while j <= RIGHT_WALL:
                     ans.append(matrix[i][j])
                     j += 1
-                i += 1
+                
                 j -= 1
+                i += 1
                 direction = DOWN
                 RIGHT_WALL -= 1
             elif direction == LEFT:
                 while j >= LEFT_WALL:
                     ans.append(matrix[i][j])
                     j -= 1
-                i -= 1
+                
                 j += 1
+                i -= 1
                 direction = UP
                 LEFT_WALL += 1
             elif direction == UP:
                 while i >= UP_WALL:
                     ans.append(matrix[i][j])
                     i -= 1
-                direction = RIGHT
+                
                 UP_WALL += 1
+                direction = RIGHT
                 i += 1
                 j += 1
             elif direction == DOWN:
-                while i < DOWN_WALL:
+                while i <= DOWN_WALL:
                     ans.append(matrix[i][j])
                     i += 1
-                
-                direction = LEFT
                 DOWN_WALL -= 1
+                direction = LEFT
                 i -= 1
                 j -= 1
-
         return ans
