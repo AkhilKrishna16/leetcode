daae81fd5f8800084d8dccdc3e28b9471aec0ce1
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2024-08-11 20:17:03 -0500

    Time: 40 ms (47.58%) | Memory: 16.4 MB (84.17%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -8,12 +8,11 @@ class Solution:
         # swap i and js
 
         for i in range(len(matrix)):
-            for j in range(i+1, len(matrix[0])):
+            for j in range(i+1, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        # reflection
+        # horizontal reflection
 
-        # for each row, swap the furthest most elements
         for i in range(len(matrix)):
             for j in range(len(matrix) // 2):
                 matrix[i][j], matrix[i][len(matrix) - j - 1] = matrix[i][len(matrix) - j - 1], matrix[i][j]
