af49101f8f67b3aa189104be34158980e29949ee
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2024-08-25 17:28:35 -0500

    Time: 62 ms (53.35%) | Memory: 17.8 MB (29.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,16 +2,19 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
 
-        # add the pairs of indices and numbers
-        # if you encounter a 'target - value = complement', add the indices to the array and return
+        # find the complement of each value, if it exists and it is not the same index as the one currently, you have your value
 
         for i, num in enumerate(nums):
             hash_map[num] = i
+        
+        # iterate through the keys and find the complement of each key's value 
+        # return the value that corresponds to the complement 
+
+        # complement = key - target
+        # if hash_map[complement] exists and hash_map[complement] != hash_map[key], return pair
 
         for i, num in enumerate(nums):
             complement = target - num
             if complement in hash_map and hash_map[complement] != i:
                 return [i, hash_map[complement]]
-
-            if complement in hash_map and hash_map[complement] != i:
-                return [i, hash_map[complement]]
+        
