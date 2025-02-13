39ddadc5ab77777331f122a8ab4cce72928ff6f0
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-02-13 10:43:42 -0800

    Time: 16 ms (70.04%) | Memory: 42.3 MB (82.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,13 +1,11 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s == null || s.length() < 1) return "";
-
         int start = 0;
         int end = 0;
 
         for(int i = 0; i < s.length(); i++) {
-            int len1 = expandUponMiddle(s, i, i);
-            int len2 = expandUponMiddle(s, i, i + 1);
+            int len1 = expandAroundMiddle(s, i, i);
+            int len2 = expandAroundMiddle(s, i, i + 1);
             int len = Math.max(len1, len2);
 
             if(len > end - start) {
@@ -19,8 +17,10 @@ class Solution {
         return s.substring(start, end + 1);
     }
 
-    public static int expandUponMiddle(String s, int a, int b) {
-        if(a > b || s == null) return 0;
+    public static int expandAroundMiddle(String s, int a, int b) {
+        if(s == null || a > b || a < 0 || b >= s.length()) {
+            return 0;
+        }
 
         while(a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b)) {
             a--;
