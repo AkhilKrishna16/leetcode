f015f7892611adc8ace9d3095b35b070d392c17f
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-07-09 01:13:20 -0500

    Time: 4 ms (90.60%) | Memory: 44.7 MB (56.93%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,23 +6,25 @@ class Solution {
         // not a duplicate and whenever it is a duplicate, keep incrementing the start
         // counter until we get rid of the duplicate
 
-        int start = 0;
-        int end = 0;
-        Set<Character> c = new HashSet<>();
+        int start = 0, end = 0;
+        Set<Character> container = new HashSet<>();
         int maxLength = 0;
-
+        
         for(end = 0; end < s.length(); end++) {
-            if(c.contains(s.charAt(end))) {
+            if(!container.contains(s.charAt(end))) {
+                container.add(s.charAt(end));
+            } else {
                 while(s.charAt(start) != s.charAt(end)) {
-                    c.remove(s.charAt(start));
+                    container.remove(s.charAt(start));
                     start++;
                 }
+
                 start++;
-            } 
-            c.add(s.charAt(end));
+            }
+
             maxLength = Math.max(maxLength, end - start + 1);
         }
 
-        return maxLength; 
+        return maxLength;
     }
 }
