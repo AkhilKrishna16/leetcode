0ef862bc0fb97d7f67e280220d9e93037c4f3685
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-08-09 11:58:53 -0500

    Time: 55 ms (38.58%) | Memory: 18.5 MB (60.12%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -3,22 +3,20 @@ class Solution:
         # go in advance of three, checking the values 
         # then iterate through the sub-matrix and find the maximum
         ret = []
-        n = len(grid)
-        for _ in range(n - 2):
-            ret.append([0] * (n - 2))
-        count = 0
-
-        for ROW in range(n - 2):
-            for COL in range(n - 2):
-                # each ROW, COL pair should represent the top left corner of my subgrid
-                maxValue = 0
-                for i in range(ROW, ROW + 3):
-                    for j in range(COL, COL + 3):
+        n = len(grid) - 2
+        for _ in range(n):
+            ret.append([-1] * n)
+        
+        for R in range(n):
+            for C in range(n):
+                maxValue = float('-inf')
+                for i in range(R, R + 3):
+                    for j in range(C, C + 3):
                         maxValue = max(maxValue, grid[i][j])
-                ret[ROW][COL] = maxValue
-                count += 1
+                ret[R][C] = maxValue
         
         return ret
+
 
 
 
