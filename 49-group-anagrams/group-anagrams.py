1632b121ab1e9e8ab59229a8633718340703838b
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-09-18 16:30:25 -0500

    Time: 7 ms (98.02%) | Memory: 20.7 MB (62.61%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # sort each string and check if strings are equal to each other 
-        # ate, eat, tea -> [aet, aet, aet], [tan, tan], [abt]
-
-        # create a hashmap for each string
-
-        res = defaultdict(list)
-
-        for string in strs:
-             count = [0] * 26 # create hash_map psuedo
-
-             for char in string:
-                count[ord(char) - ord('a')] += 1 # add occurances
-            
-             res[tuple(count)].append(string) # add the count if it exists or add existing string if it maps
+        m = defaultdict(list)
         
-        return res.values()
-             
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            m[sorted_word].append(word)
+
+        return list(m.values())
