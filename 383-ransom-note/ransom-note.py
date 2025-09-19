13e4dc6d7572f58b823ffc7df2c7f3d0f8cc0388
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-09-19 00:27:49 -0500

    Time: 15 ms (75.33%) | Memory: 17.8 MB (82.59%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_count = Counter(magazine)
+
+        for c in ransomNote:
+            if c not in magazine_count or magazine_count[c] <= 0:
+                return False
+            magazine_count[c] -= 1
+        
+        return True
