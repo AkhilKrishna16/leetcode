ef097702484038505754eb98f1c24dc2fb37301a
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-12 00:45:16 -0500

    Time: 313 ms (10.13%) | Memory: 28.4 MB (10.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,10 +2,11 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def is_valid(row, col):
             return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0]) and grid[row][col] == '1'
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        seen = set()
 
         def dfs(row, col):
+            # if not is_valid(row, col) or (row, col) in seen:
+            #     return
+            
             for dx, dy in directions:
                 i = row + dx
                 j = col + dy
@@ -14,10 +15,15 @@ class Solution:
                     dfs(i, j)
         
         ret = 0
+        seen = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if is_valid(i, j) and (i, j) not in seen:
                     ret += 1
                     seen.add((i, j))
-                    dfs(i, j) # add the starting node
+                    dfs(i, j)
+        
         return ret
+
+            
