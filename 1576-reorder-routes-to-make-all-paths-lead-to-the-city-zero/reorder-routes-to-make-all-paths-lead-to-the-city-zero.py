0f0f7e3a6d930c685180c975acdb1c8b6bf46cfd
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-12 02:42:07 -0500

    Time: 219 ms (33.97%) | Memory: 51.9 MB (40.00%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,22 +1,26 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        roads = set()
+        graph = defaultdict(list) # consists of routes you treat as unweighted
+        roads = set() # consists of the directed routes original given
 
         for x, y in connections:
             graph[x].append(y)
             graph[y].append(x)
             roads.add((x, y))
         
-        seen = {0}
+        seen = set()
         def dfs(node):
+            seen.add(node)
             ans = 0
             for v in graph[node]:
                 if v not in seen:
                     if (node, v) in roads:
-                        ans += 1
-                    seen.add(v)
+                        ans += 1 # mark the current road to be reversed
                     ans += dfs(v)
             return ans
         
         return dfs(0)
+                    
+
+
+        
