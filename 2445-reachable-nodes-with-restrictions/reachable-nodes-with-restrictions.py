26be0bd02641ab0aeab2eb1f64dcab7b686c1cad
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-13 19:10:02 -0500

    Time: 155 ms (62.76%) | Memory: 72.8 MB (58.19%) - LeetSync

--- 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.py
+++ 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.py
@@ -4,22 +4,23 @@ class Solution:
         for x, y in edges:
             graph[x].append(y)
             graph[y].append(x)
-        restricted = set(restricted)
+        
         """
         Return all nodes you can find on this path. 
         """
         seen = set()
+        seen.update(set(restricted))
         def dfs(node): 
-            if node in restricted or node in seen:
-                return 0
+            # if node in seen:
+            #     return 0
 
             seen.add(node)
 
             ans = 1
             for neighbor in graph[node]:
-                if neighbor not in seen and neighbor not in restricted:
+                if neighbor not in seen:
                     ans += dfs(neighbor)
-            
+
             return ans
         
         return dfs(0)
