ab99bc11cb433c12b90702346dc1b91a3232ffad
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-23 12:56:09 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.65%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,3 +1,13 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
+
+        # we check n > 0
+
+        # we check that the number is a power of two. this means that we only get values 
+        # with a singular bit set since powers of 4 can only have a singular bit set
+        # this goes with powers of 2,4,8,etc. because our next condition is not enough
+        # even though it truly finds the ones with bits in an odd position
+
+        # we perform and operation with n and 010101010101... to find if there are  
+        # bits set in the odd position
