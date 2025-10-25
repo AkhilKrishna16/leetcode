4d6aa33aeefcb69661df436e1ed5b483db263d96
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-25 17:24:32 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.79%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -4,7 +4,6 @@ class Solution:
         ret = float('-inf')
         for i in range(n.bit_length()):
             if n & 1 == 1:
-                print(f"{most_recent} - {i}")
                 if most_recent != None:
                     ret = max(ret, abs(i - most_recent))   
                 most_recent = i
