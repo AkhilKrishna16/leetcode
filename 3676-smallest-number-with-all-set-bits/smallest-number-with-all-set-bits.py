bb5ef5d804a2ab4ae52fe298da44550a9df51fb3
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-25 15:36:22 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (75.00%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
@@ -1,8 +1,3 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        n += 1
-        while True:
-            if bin(n).count('1') == 1:
-                return n - 1
-            n += 1
-        return -1
+        return (1 << (n.bit_length())) - 1
