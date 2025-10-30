91534c2647f4ece1a5192f1f62daaecb3f1d24ac
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-10-30 12:50:36 -0500

    Time: 3 ms (90.50%) | Memory: 21.2 MB (80.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,8 +2,8 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
         heap = []
-        for key in counter:
-            heapq.heappush(heap, (counter[key], key))
+        for key, val in counter.items():
+            heapq.heappush(heap, (val, key))
             if len(heap) > k:
                 heapq.heappop(heap)
         
