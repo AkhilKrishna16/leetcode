b3639fb1649567fd55d54e5b33d9a4ae4e0bcb39
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-11-20 23:29:25 -0600

    Time: 133 ms (67.97%) | Memory: 59.6 MB (56.27%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ret = []
-        curr_comb = []
+        curr = []
 
         def dfs(i):
-            if len(curr_comb) == k:
-                ret.append(curr_comb.copy())
-                return
+            if len(curr) == k:
+                ret.append(curr.copy())
+                return 
+            
             for j in range(i, n + 1):
-                curr_comb.append(j)
+                curr.append(j)
                 dfs(j + 1)
-                curr_comb.pop()
+                curr.pop()
         
         dfs(1)
         return ret
+
