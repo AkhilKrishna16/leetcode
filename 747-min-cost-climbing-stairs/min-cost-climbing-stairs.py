8eb5e396081ca328e7167084fb7689be4064a36d
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-11-24 03:24:10 -0600

    Time: 3 ms (73.64%) | Memory: 17.8 MB (63.12%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -26,15 +26,8 @@ class Solution:
         or the step with index 1.' Therefore, the cost of getting to these steps
         is 0. 
         """
-        memo = {}
-        def dp(i):
-            if i <= 1:
-                return 0
-            
-            if i in memo:
-                return memo[i]
-            
-            memo[i] = min(cost[i - 1] + dp(i - 1), cost[i - 2] + dp(i - 2))
-            return memo[i]
-        
-        return dp(len(cost))
+        dp = [0] * (len(cost) + 1)
+
+        for i in range(2, len(cost) + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[len(cost)]
