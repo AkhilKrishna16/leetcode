70aa9492be6b4387c87fbf459e55dee93a9c4929
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-08 00:19:54 -0600

    Time: 9 ms (78.09%) | Memory: 19.4 MB (7.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,23 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        s_count = [0] * 26
+        t_count = [0] * 26
 
-        l = {}
-        j = {}
+        for i in range(len(s)):
+            s_count[ord(s[i]) - 97] += 1
+            t_count[ord(t[i]) - 97] += 1
+        
+        for i in range(26):
+            if s_count[i] != t_count[i]:
+                return False
+        return True
 
-        # Count characters in s
-        for c in s:
-            if c not in l:
-                l[c] = 1
-            else:
-                l[c] += 1
-
-        # Count characters in t
-        for m in t:
-            if m not in j:
-                j[m] = 1
-            else:
-                j[m] += 1
-
-        # Compare the two dictionaries
-        return l == j
+        
