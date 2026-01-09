df309606135d18f51f2991f6148ab379c1c4febe
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-09 14:59:29 -0600

    Time: 63 ms (57.36%) | Memory: 31.7 MB (56.81%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,10 +1,25 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current_sum = nums[0]
-        max_sum = nums[0]
-
-        for i in range(1,len(nums)):
-            current_sum = max(nums[i], current_sum + nums[i])
-            max_sum = max(current_sum, max_sum)
+        # maybe a sliding window algorithm
+        # however, when would you decide to move the left pointer
         
-        return max_sum
+        # use a dp solution
+        # we either take the maximum sum of the previous plus the current
+        # or just the current
+
+        # so for instance,
+        # dp[0] = -2
+        # dp[1] = max(1, 1 + -2) = 1
+        # dp[2] = max(-3, 1 + -3) = -2
+        # dp[3] = max(4, 4 + -2) = 4
+        # dp[4] = max(-1, -1 + 4)
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        ret = dp[0]
+
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+            ret = max(dp[i], ret)
+        
+        return ret
