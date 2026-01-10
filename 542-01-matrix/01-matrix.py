6c861a8c30d037f8ed4c698ad279014cc83462cc
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-10 00:16:08 -0600

    Time: 162 ms (42.83%) | Memory: 23.7 MB (5.89%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,8 +1,10 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        # you could run from every 0, every cell, or every 1
+        # 
+
         def is_valid(i, j):
-            return i >= 0 and j >= 0 and i < len(mat) and j < len(mat[0]) and mat[i][j] == 1 and (i, j) not in seen
-        # add to the queue the cells with a 0
+            return i >= 0 and i < len(mat) and j >= 0 and j < len(mat[0]) and mat[i][j] == 1
         q = deque([])
         seen = set()
 
@@ -12,29 +14,20 @@ class Solution:
                     q.append((i, j, 0))
                     seen.add((i, j))
         
-        # go through the queue and add to ret
-
-        # form ret
-        # ret = []
-        # for _ in range(len(mat)):
-        #     ret.append([-1] * len(mat[0]))
-        
-        # go through the queue and mark
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        ret = [[float('inf')] * len(mat[0]) for _ in range(len(mat))]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         while q:
             n = len(q)
             for _ in range(n):
-                el = q.popleft()
-                mat[el[0]][el[1]] = el[2]
+                row, col, steps = q.popleft()
+                ret[row][col] = min(steps, ret[row][col])
 
-                # append the new elements
                 for dx, dy in directions:
-                    new_row = el[0] + dx
-                    new_col = el[1] + dy
-                    if is_valid(new_row, new_col):
+                    new_row = dx + row
+                    new_col = dy + col
+
+                    if (new_row, new_col) not in seen and is_valid(new_row, new_col):
+                        q.append((new_row, new_col, steps + 1))
                         seen.add((new_row, new_col))
-                        q.append((new_row, new_col, el[2] + 1))
-        return mat
-
-
-                
+        
+        return ret
