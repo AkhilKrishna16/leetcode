c6751a85d4ea65272f0ff22cbd86d1a890554fa3
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-12 13:17:20 -0600

    Time: 3 ms (86.70%) | Memory: 20.5 MB (8.22%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # track the indegrees of every node in numCourses
         graph = defaultdict(list)
         indeg = [0] * numCourses
 
         for a, b in prerequisites:
             graph[b].append(a)
             indeg[a] += 1
+        
         q = deque([])
         count = 0
+
         for i in range(numCourses):
             if indeg[i] == 0:
                 q.append(i)
+                count += 1
         
         while q:
             course = q.popleft()
-            # decrement the indegree of a course 
-            # if indeg[course] == 0: increment count
-            count += 1
-            
-            for resultCourse in graph[course]:
-                indeg[resultCourse] -= 1
-                if indeg[resultCourse] == 0:
-                    q.append(resultCourse)
-        
+
+            for result in graph[course]:
+                indeg[result] -= 1
+                if indeg[result] == 0:
+                    count += 1
+                    q.append(result)
         return count == numCourses
