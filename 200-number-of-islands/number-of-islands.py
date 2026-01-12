af42ddaa4b2c93c8d9e78fa7156c8925f38b1bc9
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-12 17:39:16 -0600

    Time: 268 ms (26.85%) | Memory: 21.7 MB (34.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def is_valid(row, col):
-            return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0]) and grid[row][col] == '1'
+        # start a dfs from every cell that has a 1
+        # mark everything that is connected horizontally or vertically as a 0
+        # increase count by 1 on every iteration
 
-        def dfs(row, col):
-            # if not is_valid(row, col) or (row, col) in seen:
-            #     return
-            
-            for dx, dy in directions:
-                i = row + dx
-                j = col + dy
-                if is_valid(i, j):
-                    grid[i][j] = '0'
-                    dfs(i, j)
-        
-        ret = 0
+        def is_valid(i, j):
+            return i >= 0 and i < len(grid) and j >= 0 and j < len(grid[0]) and grid[i][j] == "1"
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def dfs(i, j):
+            nonlocal directions
+            grid[i][j] = "0"
+            for dx, dy in directions:
+                new_row = dx + i
+                new_col = dy + j
+
+                if is_valid(new_row, new_col):
+                    dfs(new_row, new_col)
+
+        count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if is_valid(i, j):
-                    ret += 1
-                    grid[i][j] = '0'
+                    count += 1
                     dfs(i, j)
         
-        return ret
-
-            
+        return count
