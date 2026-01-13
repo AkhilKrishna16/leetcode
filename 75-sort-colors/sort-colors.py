23a5ce523accf279a74cb7c0442188f9ee536ac7
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-01-12 21:24:43 -0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (6.60%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,24 +1,31 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        # have one pointer marking red and one marking blue
-        # iterate through the list and follow this:
-        # 1. whenever we have a 0, move it to the front and shift the red pointer by 1 to the right
-        # 2. whenever we have a 1, move it to the back and shift the blue pointer by 1 to the left
-        # 3. whenever we have a 2, leave it ig
-        red = 0
-        blue = len(nums) - 1
-        i = 0
-        while i <= blue:
-            if nums[i] == 0:
-                nums[i], nums[red] = nums[red], nums[i]
-                red += 1
-                i = red
-            elif nums[i] == 2:
-                nums[i], nums[blue] = nums[blue], nums[i]
-                blue -= 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        RED = 0
+        WHITE = 1
+        BLUE = 2
+
+        # partition such that red goes in the front and blue marker goes in teh back
+        # 
+
+        curr = 0
+        left = 0
+        right = len(nums) - 1
+
+        while curr <= right and left < right:
+            if nums[curr] == RED:
+                nums[curr], nums[left] = nums[left], nums[curr]
+                left += 1
+                curr = left
+            elif nums[curr] == BLUE:
+                nums[curr], nums[right] = nums[right], nums[curr]
+                right -= 1
             else:
-                i += 1
+                curr += 1
         
 
-            
+
         
