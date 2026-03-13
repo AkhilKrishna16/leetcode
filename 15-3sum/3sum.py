777582706e9dd1e17de25d967ee4b14422d6b105
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-03-13 15:41:48 -0500

    Time: 629 ms (58.87%) | Memory: 22.2 MB (91.10%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,31 +1,39 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # have a two pointer solution, reduce from O(n^3) -> O(n^2)
+        # basically, every unique value starting from index 1 and going to n - 1 (exclusive)
+        # have a left pointer and a right pointer at the edges 
+        # move it together based on the rules of just adding
+        # make sure to sort first
+
+        # [-1, -1, -1, 0, 1, 2]
+        # 
+
         nums.sort()
+
         ret = []
 
-        print(nums)
-
-        for i in range(len(nums)):
-            if i > 0 and i < len(nums) and nums[i] == nums[i - 1]:
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
             left = i + 1
             right = len(nums) - 1
+
             while left < right:
                 s = nums[left] + nums[right] + nums[i]
-                if s == 0:
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
                     ret.append([nums[left], nums[right], nums[i]])
                     left += 1
                     right -= 1
-
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif s < 0:
-                    left += 1
-                else:
-                    right -= 1
-        
+                    while left < right and nums[left] == nums[left - 1]: left += 1
+                    while left < right and nums[right] == nums[right + 1]: right -= 1
         return ret
+
+                
+                    
+            
