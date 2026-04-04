ec7dba1654ebd73e76d5294d0225665e57b71bd4
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-04 01:20:33 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (56.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,27 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # if nums[mid] < nums[end], then the right half is sorted
-        # otherwise, the left half is sorted
+        # we have to find the current value's "position" respective to the ends 
+        # so for instance, if the `right` index value is greater than the current value
+        # then if the curr < target < right, then we search the right side. else search the left side 
+        # however, if the `right` index value is less than the current value
+        # then if the target > curr, search the right side. else the left side 
+
+        #   6 7 8 0 1 2 3 4 5
 
         left = 0
         right = len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            
-            if nums[mid] < nums[right]:
-                # the right half is sorted
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
+        while left < right:
+            curr = (left + right) // 2
+            if nums[curr] == target:
+                return curr
+            if nums[curr] < nums[right]:
+                if nums[curr] < target <= nums[right]:
+                    left = curr + 1
                 else:
-                    right = mid - 1
-            else:
-                # the left half is sorted
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
+                    right = curr - 1
+            elif nums[curr] > nums[right]:
+                if target <= nums[right] or target > nums[curr]:
+                    left = curr + 1
                 else:
-                    left = mid + 1
-        
+                    right = curr - 1
+        if nums[left] == target:
+            return left
         return -1
