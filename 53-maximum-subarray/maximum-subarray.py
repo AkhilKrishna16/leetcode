46eeab9f2ce4ef643b7f3a29606cc18b349d9a6a
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-07 20:50:58 -0500

    Time: 63 ms (7.39%) | Memory: 31.7 MB (12.32%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,16 +1,19 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # how do you maximize the sum
-        # [-2,1,-3,4,-1,2,1,-5,4]
+        # essentially, formulate dp array where dp[i] = max(nums[i], dp[i - 1] + nums[i])
         # dp[0] = nums[0]
-        # dp[1] = max(dp[i - 1] + nums[i], nums[i])
+        # dp[0] = -2
+        # dp[1] = 1
+        # dp[2] = -2
+        # dp[3] = 4
+        # dp[4] = 3
+        # dp[]
+        
 
         dp = [0] * len(nums)
         dp[0] = nums[0]
 
         for i in range(1, len(nums)):
-            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
         
-        # dp[n - 1] would represent the maximum subarray the ends at this point, 
         return max(dp)
-
