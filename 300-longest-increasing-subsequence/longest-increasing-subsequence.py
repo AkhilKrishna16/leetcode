ae1f454167a57d8d8ba0f2ed067abed15a3245a4
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-15 18:34:27 -0500

    Time: 1255 ms (45.06%) | Memory: 19.3 MB (88.32%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,9 +1,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # what is the recurrence? 
+        # dp[0] = 1
+        # dp[i] = max(dp[i], dp[j] + 1 if nums[j] < nums[i] else 0) for j < i
+        # return dp[len(nums) - 1]
+        # dp[i] represents the longest subsequence that ends with nums[i]
+
         dp = [1] * len(nums)
-        for i in range(len(nums)):
+         
+        for i in range(1, len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], 1 + dp[j])
-        
+
         return max(dp)
