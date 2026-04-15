0dacde868c4873a4af55ecac4c522fd3ac527381
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-15 17:54:32 -0500

    Time: 3 ms (63.10%) | Memory: 19.4 MB (55.38%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,40 +1,28 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        """
-        1. What should the function take and return? 
+        # at a certain step i, you can take the minimum of the step from two back or one back 
+        # dp[0] = cost[0] = 10
+        # dp[1] = cost[1] = 15
+        # dp[2] = cost[i] + min(dp[i - 1], dp[i - 2]) = 20 + min(10, 15) = 30
+        # dp[3] = cost[i] if i < len(cost) else 0 + min(dp[i - 1], dp[i - 2]) = 0 + min(20, 15) = 15
 
-        Talking about top-down, we need the minimum cost to climb the stairs.
-        Define `dp(state)` such that it returns the minimum cost to climb stairs
-        for a given state. 
+        # dp[0] = 1
+        # dp[1] = 100
+        # dp[2] = 1 + 1 = 2
+        # dp[3] = 1 + 2 = 3
+        # dp[4] = 1 + 2 = 3
+        # dp[5] = 100 + 3 = 103
+        # dp[6] = 1 + min(103, 3) = 4
+        # dp[7] = 1 + min(4, 103) = 5
+        # dp[8] = 100 + min(5, 4) = 104
+        # dp[9] = 1 + min(104, 5) = 6
+        # dp[10] = 0 + min(6, 104) = 6
 
-        The only state variable that is relevant is `i`, the index of what 
-        stair. Therefore, `dp(i)` should return the minimum cost to climb up
-        to the ith step. 
+        dp = [0] * (len(cost))
+        dp[0] = cost[0]
+        dp[1] = cost[1]
 
-        2. A recurrence relation to transition between states
-
-        In `fib`, it was fib[n] = fib[n - 1] + fib[n - 2]. Here, it should be
-        that to go to the 100th step (for instance), we must take the min of 
-        cost up to the 98th step + cost of the 98th step or same with 99th step. 
-        More generally, dp(i) = min(dp(i - 1) + cost[i - 1], dp(i - 2) + 
-        cost[i-2]).
-
-        3. Base case
-
-        In this case, we know the answer for this because it gives us it in 
-        the problem. 'You can either start from the step with index 0, 
-        or the step with index 1.' Therefore, the cost of getting to these steps
-        is 0. 
-        """
-        memo = {}
-        def dp(i):
-            if i == 0 or i == 1:
-                return 0
-            if i in memo:
-                return memo[i]
-             
-            memo[i] = min(dp(i - 2) + cost[i - 2], dp(i - 1) + cost[i - 1])
-            return memo[i]
-        return dp(len(cost))
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
         
-        # the cost of getting to the nth step which is the top since you must take all the stairs leading up to this "step" or
+        return min(dp[-1], dp[-2])
