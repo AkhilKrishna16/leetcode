7d6fc0f794d3cfe6a3bff3c79cfaa20342544ba6
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-15 18:10:54 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.79%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
+        # you have a choice at each house that you are on:
+        # take the current and take the total rewards from two houses ago
+        # or take the previous house total rewards
+        # base case: since we are going back max two steps, we need to have a case for 0 AND 1
+        # so for dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
+        # return dp[len(nums) - 1]
+        if len(nums) < 2:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        def dp(i):
-            if i == 0:
-                return nums[0]
-            elif i == 1:
-                return max(nums[0], nums[1])
-            
-            if i in memo:
-                return memo[i]
-            
-            memo[i] = max(dp(i - 2) + nums[i], dp(i - 1))
-            return memo[i]
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
         
-        return dp(len(nums) - 1)
+        return dp[-1]
+
