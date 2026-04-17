f5e4fd168ce5d12d51c2d874289796682ae927c9
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-16 22:28:49 -0500

    Time: 66 ms (55.94%) | Memory: 56.1 MB (46.60%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,15 +1,21 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        n = len(questions)
-        dp = [0] * n
-        dp[n - 1] = questions[n - 1][0]
+        # so you can probably solve it in a reverse-style dp 
+        # essentially, you start from the end
+        # dp[i] = max amount of points that can be earned using this question and onwards
+        # dp[len(questions) - 1] = questions[-1][0] = 2
+        # dp[len(questions) - 2] = max(dp[len(questions) - 1], questions[-2][0] + dp[questions[-2 + i + 1][1]])
+        # = max(2, 4) = 4
+        # dp[-3] = max(4, 4) = 4
+        # dp[-4] = max(4, 3 + 2) = 5   
+        # return dp[0]
+        # dp[i] = max(dp[i + 1], questions[i][0] + dp[i + 1 + questions[i][1]])
 
-        for i in range(n - 2, -1, -1):
-            j = i + questions[i][1] + 1
+        dp = [0] * len(questions)
+        dp[-1] = questions[-1][0]
 
-            if j >= len(questions):
-                dp[i] = max(questions[i][0], dp[i + 1])
-            else:
-                dp[i] = max(questions[i][0] + dp[j], dp[i + 1])
+        for i in range(len(questions) - 2, -1, -1):
+            add = dp[i + 1 + questions[i][1]] if i + 1 + questions[i][1] < len(questions) else 0
+            dp[i] = max(dp[i + 1], questions[i][0] + add)
 
         return dp[0]
