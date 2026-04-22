73dbf716693c5a133f940ff6232d6df5d5680552
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-21 19:46:59 -0500

    Time: 1 ms (13.23%) | Memory: 19.3 MB (71.84%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,14 +1,26 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        dp = [[] for i in range(numRows)]
-        dp[0] = [1]
+        # dp[0] = [1]
+        # dp[1] = [1, 1]
+        # dp[2] = [1, 2, 1]
+        # dp[3] = [1, 3, 3, 1]
+        # dp[4] = [1, 4, 6, 4, 1]
+        # it can just be simulation
+        # use the previous problem to solve the current
+        # essentially, iterate in a for loop style: outer loop runs numsRows times
+        # then the inner loop appends one, then runs i - 2 times 
+        # u would run [1, [2], 1] -> [1, 3, 3, 1] for the inner value
+        # then append 1 again
 
-        for i in range(1, numRows):
-            dp[i] = [0] * (i + 1)
-            dp[i][0] = 1
-            dp[i][i] = 1
+
+        dp = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
             for j in range(1, i):
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-
+                row[j] = dp[i - 1][j - 1] + dp[i - 1][j]
+            dp.append(row)
         
         return dp
+                
+        
