a8308ddead0b1010b73799be876e61982bb2158f
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-21 20:50:12 -0500

    Time: 35 ms (9.02%) | Memory: 19.1 MB (82.93%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -1,18 +1,25 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        # have a dp array such that 
-        # dp[i] = whether Alice can win given a number n
+        # essentially, you would just create a dp array of x + 1 length
+        # then find all values less than i that are less than x
+        # dp[i] &= !dp[i - x] for every x where n % x == 0
+        # dp[0] = False -> there are no moves from this point; just losing
+        # dp[1] = False
+        # dp[2] = True -> you are gauranteed a win from here
+        # dp[3] = False -> in this case, there is a True at dp[2] so you are losing here
+        # dp[4] = True -> you can pick 1 as x so 4 - 1 = 3 and dp[3] which is False meaning there is a losing position 
+        # dp[5] = False -> the only move is 1 which leads to dp[4] which is True 
 
-        # dp[0] = True
-        # dp[1] = True
-        # dp[2] &= dp[x] such that n' is divisible by x
-        # dp[2] &= dp[1] = True
-        # dp[3] &= dp[2] = True
-
+        # you just have to check if there is a move that is False at an x that is n is divisible by.  
+        
         dp = [False] * (n + 1)
-        for i in range(2, n + 1):
-            for j in range(1, i):
-                if i % j == 0:
-                    dp[i] |= not dp[i - j]
+
+        for i in range(1, n + 1):
+            for x in range(1, i):
+                if i % x == 0 and not dp[i - x]:
+                    dp[i] = True
+                    break
+
         return dp[n]
+
         
