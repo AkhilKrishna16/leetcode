6c99318b203b589ca7f95b32ccbb78f03f145ace
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-04-30 22:34:25 -0500

    Time: 1032 ms (16.80%) | Memory: 44.5 MB (38.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -9,25 +9,18 @@ class Solution:
         # dp[1][1] = 1
         # dp[2][0] = 1
         # dp[2][1] = 1 + 1 = 2
-        dp = []
-        for i in range(len(text1)):
-            dp.append([0] * len(text2))
+        
+        m = len(text1)
+        n = len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        dp[0][0] = 1 if text1[0] == text2[0] else 0
-        for i in range(len(text1)):
-            dp[i][0] = max(dp[i - 1][0], 1 if text1[i] == text2[0] else 0)
-        
-        for j in range(len(text2)):
-            dp[0][j] = max(dp[0][j - 1], 1 if text1[0] == text2[j] else 0)
-        
-        for i in range(1, len(text1)):
-            for j in range(1, len(text2)):
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
                 if text1[i] == text2[j]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    dp[i][j] = dp[i + 1][j + 1] + 1
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-        return dp[-1][-1]
-                
-
+                    print(i, j)
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        
+        return dp[0][0]
 
