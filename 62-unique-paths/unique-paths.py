c0a3fc9b51d158f8aeb2c4225e0f6f865d9144f2
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-05-02 03:50:49 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (42.72%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,28 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # essentially, dp[m][n] = # of ways to get to [m][n] from 0, 0
-        # dp[i][j] = dp[i - 1][j] + dp[i][j - 1] if i - 1 not out of bounds and j - 1 not out of bounds for respective
-        # dp[0][0] = 1
-
-        dp = []
-
-        for i in range(m):
-            dp.append([0] * (n))
+        # dp[i][j] = the number of unique paths to cell i,j
+        # return dp[m - 1][n - 1]
+        # start from 1, 1
+        # dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
         
-        dp[0][0] = 1
+        dp = [[0 for _ in range(n)] for _ in range(m)]
 
         for i in range(m):
             dp[i][0] = 1
         for j in range(n):
             dp[0][j] = 1
-
-
+        
         for i in range(1, m):
             for j in range(1, n):
-                if i - 1 >= 0:
-                    dp[i][j] += dp[i - 1][j]
-                if j - 1 >= 0:
-                    dp[i][j] += dp[i][j - 1]
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         
         return dp[m - 1][n - 1]
-
