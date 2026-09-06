bb8ea8ec5d879fb6e224a2670f5de1b29c4c659c
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-09-05 21:19:47 -0500

    Time: 243 ms (58.41%) | Memory: 21.5 MB (92.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # perform a dfs from every one you find. mark every one in this component as a 0, everything adjacent in a chain
-        # should be o(n^2)
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        def dfs(grid, row, col):
-            grid[row][col] = 0
+        # every cell that is a one, we can dfs and mark everything as a 0
+        def dfs(grid, i, j):
+            grid[i][j] = '0'
+
+            directions = [(1,0), (0,1), (-1,0), (0,-1)]
+
             for dx, dy in directions:
-                new_row = dx + row
-                new_col = dy + col
+                new_row = dx + i
+                new_col = dy + j
 
-                if new_row >= 0 and new_row < len(grid) and new_col >= 0 and new_col < len(grid[0]) and grid[new_row][new_col] == "1":
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col] == '1':
                     dfs(grid, new_row, new_col)
-
-        components = 0
+        count = 0
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    components += 1
+                if grid[i][j] == '1':
+                    count += 1
                     dfs(grid, i, j)
-        
-        return components
+            
+        return count
