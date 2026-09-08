4e11170d3a691fb9b5404b5e66762584b2640fcc
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-09-08 16:43:30 -0500

    Time: 3 ms (75.39%) | Memory: 19.3 MB (79.22%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,34 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # essentially, if the count that has been processed, the number of fresh -> turned to rotten + empty is m x n, ur done.
-        # find all rotten ones. add to queue. 
-        # then you go through minute by minute
-        # 
-
-        q = deque([])
-        ret = 0
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        q = deque()
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     q.append((i, j, 0))
-        
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        max_min = 0
         while q:
-            row, col, minutes = q.popleft()
-            ret = minutes
+            i, j, mins = q.popleft()
+            max_min = mins
 
             for dx, dy in directions:
-                new_row = row + dx
-                new_col = col + dy
-                if new_row >= 0 and new_row < len(grid) and new_col >= 0 and new_col < len(grid[0]) and grid[new_row][new_col] == 1:
+                new_row = i + dx
+                new_col = j + dy
+                
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col] == 1:
                     grid[new_row][new_col] = 2
-                    q.append((new_row, new_col, minutes + 1))
+                    q.append((new_row, new_col, mins + 1))
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
+        for row in grid:
+            for col in row:
+                if col == 1:
                     return -1
+        return max_min 
+
         
-        return ret
-            
