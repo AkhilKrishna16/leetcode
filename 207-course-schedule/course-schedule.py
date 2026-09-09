32da1f644f30e2ed90e6e87828c439d4fed396b8
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-09-09 09:46:44 -0500

    Time: 6 ms (49.06%) | Memory: 20.2 MB (98.71%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,42 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # basically, create a graph where prereqs[i][0] -> prereqs[i][1]
-        # if len(visited) == numCourses: return True
-        #  else: return False
-
-        # it is cycle detection
-        # if there is a cycle, you can't take some classes
-        # also if there is no connection to something, then you can't take 
-
+        indegree = [0] * (numCourses)
         graph = defaultdict(list)
-        indegree = {i: 0 for i in range(numCourses)}
 
-        for course, prereq in prerequisites:
-            indegree[course] += 1
-            graph[prereq].append(course)
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
         
-        # start from every course with value greater than 0 
-        q = deque([])
-        
-        for k in indegree:
-            if indegree[k] == 0:
-                q.append(k)
-        
+        q = deque()
+
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                q.append(i)
         while q:
             course = q.popleft()
 
             for neighbor in graph[course]:
                 indegree[neighbor] -= 1
-
                 if indegree[neighbor] == 0:
                     q.append(neighbor)
         
-        for k in indegree:
-            if indegree[k] > 0:
+        for indegree_count in indegree:
+            if indegree_count > 0:
                 return False
         
         return True
-                    
             
+        
 
+            
 
